e05f83eaeb3f32a37ac4a0ddd96a1673b21f1d4b
Yechiel Vogel <yechielvogel@gmail.com>
2023-07-25 20:01:37 -0400

    build (5)

---
+++ flutter_application_1/lib/globals.dart
@@ -0,0 +1,23 @@
+library my_project.globals;
+
+int team1Score = 0;
+int team2Score = 0;
+int score1 = 0;
+int score2 = 0;
+int teamIndex1 = 0;
+int teamIndex2 = 0;
+List<String> teamNames = [
+  'Nemenov Knights',
+  'Paritcher Poachers',
+  'Rashbatz Riders',
+  'Charitonov Chasers',
+  'Kesselman Kings',
+  'Althoise Archers',
+  'Pariz parkers',
+  'Kletzker Crackers',
+  'Futerfas Fighters',
+  'Maharil Marchers',
+  'Munkis Markes',
+  'Dvorkin Dodgers',
+  // Add more names here
+];
